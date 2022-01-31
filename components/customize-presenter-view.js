4adcada6f8a6c1ef45b46b366f7d9f68179a6972
Dakota L Martinez <dakotaleemusic@gmail.com>
2022-01-31 15:23:04 -0800

    add keyboard shortcut for resizing the notes pane in presenter mode

--- components/customize-presenter-view.js
+++ components/customize-presenter-view.js
@@ -2,19 +2,15 @@ import React, { useEffect } from 'react';
 const CustomizePresenterView = () => {
   useEffect(() => {
     document.addEventListener('keydown', (e) => {
-      if (e.altKey && e.code === 'KeyP') {
-        let nextSlide = document.querySelector('#gatsby-focus-wrapper > div > div:last-child > div:first-child');
-        if (nextSlide && nextSlide.className.split('-')[2] === 'Presenter') {
-          nextSlide.parentElement.append(nextSlide);
-          let notes = document.querySelector('#gatsby-focus-wrapper > div > div:last-child > div:first-child');
-          if (notes.className.split('-')[2] === 'Presenter') {
-            notes.style.height = '200px';
-            notes.style.overflow = 'scroll';
-            notes.style.display = 'block';
-            notes.style.flexGrow = 0;
-            notes.style.overflowX = 'hidden';
-          }
-        }
+      if (e.altKey && e.code === 'KeyN') {
+        let nextSlide = document.querySelector('#gatsby-focus-wrapper > div > div:last-child > div:first-child'); 
+        nextSlide.parentElement.append(nextSlide);
+        let notes = document.querySelector('#gatsby-focus-wrapper > div > div:last-child > div:first-child');
+        notes.style.height = '200px';
+        notes.style.overflow = 'scroll';
+        notes.style.display = 'block';
+        notes.style.flexGrow = 0;
+        notes.style.overflowX = 'hidden';
       }
     })
   })
